20eada282547a6f6906f3ebb166c1e9ff1844ec1
Andreas Bigger <bigger@usc.edu>
2020-03-30 23:14:16 -0700

    initital commit 🚀

---
+++ src/main/java/CS201/Vinder/VinderApplication.java
@@ -0,0 +1,13 @@
+package CS201.Vinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CS201/Vinder/VinderApplicationTests.java
@@ -0,0 +1,13 @@
+package CS201.Vinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
