30b44afeab37f5beeea1af2b5cf9ea10ae4015d4
Andreas Bigger <bigger@usc.edu>
2020-03-31 12:39:22 -0700

    arrays and json return types for Java Spring REST API? :question:

--- src/main/java/CS201/Vinder/IndexController.java
+++ src/main/java/CS201/Vinder/IndexController.java
@@ -1,6 +1,6 @@
 package CS201.Vinder;
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
